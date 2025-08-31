6d30b6b6d9f6e1fbe088cf183e999e669423f309
Aris Karamat <karamataris@gmail.com>
2025-08-31 16:02:29 +0200

    added Navbar

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import ReactDOM from "react-dom/client";
+import Navbar from "./components/navbar";
 
-const element = <h1>Hello World</h1>;
+
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(element);
+root.render(<Navbar/>);
